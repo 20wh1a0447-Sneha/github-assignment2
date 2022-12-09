ca8c4617efc63d71e938272711ce86e1b176d55c
20wh1a0447-Sneha <119689250+20wh1a0447-Sneha@users.noreply.github.com>
2022-12-09 07:43:49 +0530

    Add files via upload

---
+++ prog10 branching.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int wavelength;
+    cin>>wavelength;
+    cout<<"enter wavelength range:";
+    if(wavelength < 350 && wavelength <= 450){
+        cout<<"violet";
+    }
+    else if(wavelength < 450 && wavelength <=495){
+        cout<<"Blue";
+    }
+    else if(wavelength < 495 && wavelength <=570){
+        cout<<"Green";
+    }
+        else if(wavelength < 570 && wavelength <=590){
+            cout<<"Yellow";
+        }
+    else if(wavelength < 590 && wavelength <=620){
+        cout<<"Orange";
+    }
+        else if(wavelength < 620 && wavelength <=750){
+            cout<<"Red";
+        }
+        else
+            cout<<"invalid";
+        return 0;
+}
