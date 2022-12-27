20f019b3965fb10786073868853cd0c808ae6d9e
20wh1a0447-Sneha <119689250+20wh1a0447-Sneha@users.noreply.github.com>
2022-12-27 19:52:02 +0530

    Add files via upload

---
+++ prog8.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	int i;
+	string s;
+	cin>>i>>s;
+	if((s=="december"&&i>=22)||(s=="january"&&i<=19)){
+		cout<<"capricon";
+	}
+	else if((s=="january"&&i>=20)||(s=="february"&&i<=18)){
+		cout<<"aquarius";
+	}
+	else if((s=="february"&&i>=19)||(s=="march"&&i<=20)){
+		cout<<"pisces";
+	}
+	else if((s=="march"&&i>=21)||(s=="april"&&i<=19)){
+		cout<<"aries";
+	}
+	else if((s=="april"&&i>=20)||(s=="may"&&i<=20)){
+		cout<<"taurus";
+	}
+	else if((s=="may"&&i>=21)||(s=="june"&&i<=20)){
+		cout<<"gemini";
+	}
+	else if((s=="june"&&i>=21)||(s=="july"&&i<=22)){
+		cout<<"cancer";
+	}
+	else if((s=="july"&&i>=23)||(s=="august"&&i<=22)){
+		cout<<"leo";
+	}
+	else if((s=="august"&&i>=23)||(s=="september"&&i<=22)){
+		cout<<"virgo";
+	}
+	else if((s=="september"&&i>=23)||(s=="october"&&i<=22)){
+		cout<<"libra";
+	}
+	else if((s=="october"&&i>=23)||(s=="november"&&i<=21)){
+		cout<<"scorpio";
+	}
+	else if((s=="november"&&i>=22)||(s=="december"&&i<=21)){
+		cout<<"sagarittarius";
+	}
+	return 0;
+}
